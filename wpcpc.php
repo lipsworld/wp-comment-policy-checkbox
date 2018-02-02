fd2c1ed7a23273153001508198332c854646a446
fcojgodoy <francisco.godoy@openmailbox.org>
2018-02-02 01:26:24 +0100

    fix generic function (and/or define) names

--- wpcpc.php
+++ wpcpc.php
@@ -30,13 +30,6 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 0.1.1 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '0.1.1' );
-
 
 /**
  * Admin
